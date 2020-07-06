986df313f8d1c0de4074835293ee3e283d0e65c4
Johannes Schriewer <hallo@dunkelstern.de>
2020-07-06 16:42:48 +0200

    Update to 1.1.2

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ with open("README.md", "r") as fd:
 
 setuptools.setup(
     name="wagtail_to_ion",
-    version="1.1.1",
+    version="1.1.2",
     author="anfema GmbH",
     author_email="admin@anfe.ma",
     description="Wagtail to ION API adapter",
