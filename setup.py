de4fe612e21b733be9f24fcc083942851b27e572
Johannes Schriewer <hallo@dunkelstern.de>
2020-07-07 14:21:23 +0200

    Bump version to 1.1.5

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ with open("README.md", "r") as fd:
 
 setuptools.setup(
     name="wagtail_to_ion",
-    version="1.1.4",
+    version="1.1.5",
     author="anfema GmbH",
     author_email="admin@anfe.ma",
     description="Wagtail to ION API adapter",
