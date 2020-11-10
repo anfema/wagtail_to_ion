c615b3e61e992215b96a53684c9bafe6bd3d13ae
f.plett <f.plett@anfe.ma>
2020-11-10 19:00:27 +0100

    Fixed media and image delete urls
    Re #1

--- wagtail_to_ion/urls/wagtail_override_urls.py
+++ wagtail_to_ion/urls/wagtail_override_urls.py
@@ -1,14 +1,15 @@
 # Copyright © 2017 anfema GmbH. All rights reserved.
 from django.conf.urls import url
+from django.urls import path
 from wagtail.utils.urlpatterns import decorate_urlpatterns
 from wagtail.admin.auth import require_admin_access
 from wagtail_to_ion.views.wagtail_override import delete_image, image_index, delete_media, add_image, add_media, document_index, add_document, ion_create, ion_edit, ion_unpublish,ion_publish_with_children
 
 
 urlpatterns = [
-    url(r'^images/<int:image_id>/delete/$', delete_image),
-    url(r'^images/$', image_index , name='image-index'),
-    url(r'^media/delete/<int:media_id>/$', delete_media),
+    path('images/<int:image_id>/delete/', delete_image),
+    url(r'^images/$', image_index, name='image-index'),
+    path('media/delete/<int:media_id>/', delete_media),
     url(r'^images/multiple/add/$', add_image, name='add_multiple'),
     url(r'^media/(\w+)/add/$', add_media, name='add'),
     url(r'^documents/$', document_index , name='document-index'),
