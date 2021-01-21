20084018d7a8d626a917d774875a4b776814b392
Ferdinand Hoffmann <f.hoffmann@anfe.ma>
2021-01-21 13:56:10 +0100

    Remove collection related overrides

--- wagtail_to_ion/views/wagtail_override/__init__.py
+++ wagtail_to_ion/views/wagtail_override/__init__.py
@@ -1,4 +1,1 @@
-from .file_based_models import add_image, delete_image, add_media, delete_media, add_document
 from .pages import ion_unpublish, ion_publish_with_children
-from .image_index import image_index
-from .document_index import document_index
--- wagtail_to_ion/wagtail_hooks/show_only_user_documents.py
+++ wagtail_to_ion/wagtail_hooks/show_only_user_documents.py
@@ -1,8 +1,9 @@
-from django.urls import reverse
-
 from wagtail.core import hooks
 
 from wagtail_to_ion.utils import get_user_documents
+
+
+# TODO: remove (should be obsolete once 'choose' permission is available; permission handling is project specific)
 
 
 @hooks.register('construct_document_chooser_queryset')
@@ -11,9 +12,3 @@ def show_only_user_documents(documents, request):
     Only show images in user collections
     """
     return get_user_documents(request.user, documents)
-
-@hooks.register('construct_main_menu')
-def modify_documents_menu_item(request, menu_items):
-    for item in menu_items:
-        if item.name == 'documents':
-            item.url = reverse('document-index')
