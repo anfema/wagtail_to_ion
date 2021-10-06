7601727ec567175356208cededef1ee79c74d94c
Johannes Schriewer <j.schriewer@anfe.ma>
2021-10-06 16:43:27 +0200

    Bugfix: Only append variation if internal urls are used

--- wagtail_to_ion/serializers/tar.py
+++ wagtail_to_ion/serializers/tar.py
@@ -147,7 +147,7 @@ def make_page_tar(page, locale, request, content_serializer=DynamicPageDetailSer
         f['tar_name'] = "pages/" + f["page"] + "/" + str(i[f["page"]])
         i[f["page"]] = i[f["page"]] + 1
         url = f["url"]
-        if "variation" in request.GET:
+        if "variation" in request.GET and url.startswith(settings.BASE_URL):
             url += "?variation=" + request.GET["variation"]
         index_file.append({
             "url": url,
